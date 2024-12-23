ecdaa4361411e30c23bfa9b82539f16cd594e198
Sean P. Kelly <seankell@amazon.com>
2024-12-23 19:04:46 +0000

    twoliter: refactor project to reduce module length
    The project module is large, making it difficult to organize and read.
    This moves much of the vended image/artifact machinery into a new
    module. We also add a new trait to help us write functionality over
    both Projects that are fully locked and Projects with a locked SDK.

--- twoliter/src/project/lock/mod.rs
+++ twoliter/src/project/lock/mod.rs
@@ -13,12 +13,13 @@ mod verification;
 mod views;
 
 pub(crate) use self::verification::VerificationTagger;
+pub(crate) use image::LockedImage;
 
 use crate::common::fs::{create_dir_all, read, write};
 use crate::project::{Project, ValidIdentifier};
 use crate::schema_version::SchemaVersion;
 use anyhow::{bail, ensure, Context, Result};
-use image::{ImageResolver, LockedImage};
+use image::ImageResolver;
 use oci_cli_wrapper::ImageTool;
 use olpc_cjson::CanonicalFormatter as CanonicalJsonFormatter;
 use semver::Version;
