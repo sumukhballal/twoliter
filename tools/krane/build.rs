3c9247345a94ec4f316c08f158a1108ebfdd6f4c
Sam Berning <bernings@amazon.com>
2025-01-14 22:17:50 +0000

    Revert "krane-static: harden CFLAGS used to build krane-static"
    This reverts commit c9b044262137c66c74d8a9fc6c9f1e5dfb34d4e6.

--- tools/krane/build.rs
+++ tools/krane/build.rs
@@ -3,11 +3,6 @@ use std::path::PathBuf;
 use std::process::Command;
 
 const REQUIRED_TOOLS: &[&str] = &["go"];
-const CFLAGS: &str = concat!(
-    "-O2 -g -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS ",
-    "-fexceptions -fstack-clash-protection -fno-omit-frame-pointer",
-);
-const LDFLAGS: &str = "-Wl,-z,relro -Wl,-z,now";
 
 fn main() {
     let script_dir = env::current_dir().unwrap();
@@ -21,22 +16,10 @@ fn main() {
     let build_output_loc = out_dir.join("libkrane.a");
     let mut build_command = Command::new("go");
 
-    let curr_cflags = env::var("CFLAGS").unwrap_or_default();
-    let desired_cflags = format!("{curr_cflags} {CFLAGS}");
-
-    let curr_ldflags = env::var("LDFLAGS").unwrap_or_default();
-    let desired_ldflags = format!("{curr_ldflags} {LDFLAGS}");
-
     build_command
         .env("GOOS", get_goos())
         .env("GOARCH", get_goarch())
         .env("CGO_ENABLED", "1")
-        .env("CFLAGS", &desired_cflags)
-        .env("CGO_CFLAGS", &desired_cflags)
-        .env("CXXFLAGS", &desired_cflags)
-        .env("CGO_CXXFLAGS", &desired_cflags)
-        .env("LDFLAGS", &desired_ldflags)
-        .env("CGO_LDFLAGS", &desired_ldflags)
         .arg("build")
         .arg("-buildmode=c-archive")
         .arg("-o")
